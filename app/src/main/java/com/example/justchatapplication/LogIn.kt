691b14972ed40fcd7aaeb3f8375ab6f0d60f6957
meetkishan <meetkachchhi@gmail.com>
2022-03-16 18:17:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/justchatapplication/LogIn.kt
@@ -0,0 +1,66 @@
+package com.example.justchatapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class LogIn : AppCompatActivity() {
+
+    private lateinit var edtEmail : EditText
+    private lateinit var edtPassword : EditText
+    private lateinit var btnLogIn : Button
+    private lateinit var btnSignUp : Button
+    private lateinit var mAuth : FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+
+
+        mAuth = FirebaseAuth.getInstance()
+
+
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnLogIn = findViewById(R.id.btnLogin)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+
+        btnSignUp.setOnClickListener {
+
+            val intent = Intent(this,SignUp::class.java)
+            startActivity(intent)
+        }
+
+        btnLogIn.setOnClickListener {
+
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+
+
+            login(email,password)
+        }
+    }
+
+    private fun login(email: String, password: String) {
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+
+                    val intent = Intent(this@LogIn,MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                    Toast.makeText(this@LogIn, "User does not exist", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+    }
+}
