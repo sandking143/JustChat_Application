49d24b71cd1f6b70deb21a4466ad9788a97bef8f
meetkishan <meetkachchhi@gmail.com>
2022-03-17 16:54:08 +0530

    day 2 - 4.50

--- app/src/main/java/com/example/justchatapplication/MainActivity.kt
+++ app/src/main/java/com/example/justchatapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.justchatapplication
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,6 +9,7 @@ import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,12 +17,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var userList: ArrayList<User>
     private lateinit var adapter: UserAdapter
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mAuth = FirebaseAuth.getInstance()
+        mDbRef = FirebaseDatabase.getInstance().getReference()
 
 
         userList = ArrayList()
@@ -31,6 +35,31 @@ class MainActivity : AppCompatActivity() {
         userRecyclerView.layoutManager = LinearLayoutManager(this)
 
         userRecyclerView.adapter = adapter
+
+        mDbRef.child("user").addValueEventListener(object : ValueEventListener {
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                userList.clear()
+                for (postSnapshot in snapshot.children) {
+                    val currentUser = postSnapshot.getValue(User::class.java)
+
+                    if (mAuth.currentUser?.uid != currentUser?.uid) {
+
+                        userList.add(currentUser!!)
+                    }
+
+                }
+
+                adapter.notifyDataSetChanged()
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
 
     }
 
@@ -43,7 +72,7 @@ class MainActivity : AppCompatActivity() {
 
         if (item.itemId == R.id.logout) {
             mAuth.signOut()
-            val intent = Intent(this@MainActivity,LogIn::class.java)
+            val intent = Intent(this@MainActivity, LogIn::class.java)
             finish()
             startActivity(intent)
             return true
