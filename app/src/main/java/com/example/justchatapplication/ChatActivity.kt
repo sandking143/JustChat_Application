ef6e0fe97ea60daf1c86ad38fa21bd2937d233c1
meetkishan <meetkachchhi@gmail.com>
2022-03-17 18:48:55 +0530

    day 2 - 6.37

--- app/src/main/java/com/example/justchatapplication/ChatActivity.kt
+++ app/src/main/java/com/example/justchatapplication/ChatActivity.kt
@@ -1,15 +1,16 @@
 package com.example.justchatapplication
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 
 class ChatActivity : AppCompatActivity() {
 
@@ -44,9 +45,34 @@ class ChatActivity : AppCompatActivity() {
         chatRecyclerView = findViewById(R.id.chatRecyclerView)
         messageBox = findViewById(R.id.messageBox)
         sendButton = findViewById(R.id.sentButton)
-
         messageList = ArrayList()
         messageAdapter = MessageAdapter(this,messageList)
+
+        chatRecyclerView.layoutManager = LinearLayoutManager(this)
+        chatRecyclerView.adapter = messageAdapter
+
+
+        mDbRef.child("chats").child(senderRoom!!).child("messages")
+            .addValueEventListener(object: ValueEventListener{
+                @SuppressLint("NotifyDataSetChanged")
+                override fun onDataChange(snapshot: DataSnapshot) {
+
+                    messageList.clear()
+
+                    for (postSnapshot in snapshot.children){
+
+                        val message = postSnapshot.getValue(Message::class.java)
+                        messageList.add(message!!)
+                    }
+                    messageAdapter.notifyDataSetChanged()
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            })
 
         sendButton.setOnClickListener {
             val message = messageBox.text.toString()
@@ -59,6 +85,8 @@ class ChatActivity : AppCompatActivity() {
                     mDbRef.child("chats").child(receiverRoom!!).child("messages").push()
                         .setValue(messageObject)
                 }
+
+            messageBox.setText("")
 
 
         }
