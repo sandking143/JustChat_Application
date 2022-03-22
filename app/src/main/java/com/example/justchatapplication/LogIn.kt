f653d63a9e57b213c1b27cbf6f0f3f4b19b7eea6
meetkishan <meetkachchhi@gmail.com>
2022-03-22 17:20:09 +0530

    day 4 - 6.45

--- app/src/main/java/com/example/justchatapplication/LogIn.kt
+++ app/src/main/java/com/example/justchatapplication/LogIn.kt
@@ -1,12 +1,17 @@
 package com.example.justchatapplication
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+
 
 class LogIn : AppCompatActivity() {
 
@@ -15,10 +20,23 @@ class LogIn : AppCompatActivity() {
     private lateinit var btnLogIn : Button
     private lateinit var btnSignUp : Button
     private lateinit var mAuth : FirebaseAuth
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
+    private lateinit var checkBox: CheckBox
+    private lateinit var strName: String
+    private lateinit var strPassword: String
+    private lateinit var strCheckBox: String
 
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        editor = sharedPreferences.edit()
+
 
 
         supportActionBar?.hide()
@@ -31,6 +49,7 @@ class LogIn : AppCompatActivity() {
         edtPassword = findViewById(R.id.edt_password)
         btnLogIn = findViewById(R.id.btnLogin)
         btnSignUp = findViewById(R.id.btnSignUp)
+        checkBox = findViewById(R.id.checkBox)
 
 
         btnSignUp.setOnClickListener {
@@ -45,9 +64,31 @@ class LogIn : AppCompatActivity() {
             val password = edtPassword.text.toString()
 
 
+            if (checkBox.isChecked){
+                editor.putString(getString(R.string.checkBox), "True")
+                editor.apply()
+                strName = edtEmail.text.toString()
+                editor.putString(getString(R.string.edtEmail), strName)
+                editor.commit()
+                strPassword = edtPassword.text.toString()
+                editor.putString(getString(R.string.edtPassword), strPassword)
+                editor.commit()
+
+            }else {
+                editor.putString(getString(R.string.checkBox), "False")
+                editor.commit()
+                editor.putString(getString(R.string.edtEmail), "")
+                editor.commit()
+                editor.putString(getString(R.string.edtPassword), "")
+                editor.commit()
+            }
+
 
             login(email,password)
+
+
         }
+        checkSharedPreference()
     }
 
     private fun login(email: String, password: String) {
@@ -65,6 +106,16 @@ class LogIn : AppCompatActivity() {
                     Toast.makeText(this@LogIn, "User does not exist", Toast.LENGTH_SHORT).show()
                 }
             }
+
+    }
+    private fun checkSharedPreference() {
+        strCheckBox = sharedPreferences.getString(getString(R.string.checkBox), "False").toString()
+        strName = sharedPreferences.getString(getString(R.string.edtEmail), "").toString()
+        strPassword = sharedPreferences.getString(getString(R.string.edtPassword), "").toString()
+        edtEmail.setText(strName)
+        edtPassword.setText(strPassword)
+        checkBox.isChecked = strCheckBox == "True"
+
 
     }
 }
